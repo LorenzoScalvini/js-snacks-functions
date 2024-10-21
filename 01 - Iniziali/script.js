303cbe723ce40f0d5e4e1f9660a832b367576bd5
LorenzoScalvini <lorenzo.scalvini1704@gmail.com>
2024-10-21 15:45:52 +0200

    ADD: esercizio 1

---
+++ 01 - Iniziali/script.js
@@ -0,0 +1,30 @@
+/* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
+
+const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
+
+// Dichiara la funzione qui.
+function getFirstChar(names) {
+  const firstChar = []; // Crea un array vuoto per memorizzare le iniziali
+
+  // Costrutto for che cicla attraverso ogni nome nell'array di nomi
+  for (let i = 0; i < names.length; i++) {
+    const name = names[i]; // Prendi il nome corrente
+    const currentElement = name.split(" "); // Divide il nome in parole
+    let firstNameChars = ""; // Inizializza una stringa per memorizzare le iniziali del nome corrente
+
+    // Cicla attraverso ogni parola nel nome
+    for (let j = 0; j < currentElement.length; j++) {
+      firstNameChars += currentElement[j].charAt(0);
+    }
+    firstChar.push(firstNameChars);
+  }
+  // Restituisce l'array contenente tutte le iniziali
+  return firstChar;
+}
+// Invoca la funzione qui e stampa il risultato in console
+const initialsArray = getFirstChar(names);
+console.log(names);
+console.log(initialsArray);
+
+//Risultato atteso: ["A", "L", "M", "A", "G", "A"]
+//Pero' un ora di dario moccia andava bene, non mi dispiaceva
